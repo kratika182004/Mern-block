f6f1d2313a6ddc3660b8284964fc6e3b81656ed7
Kratika Gupta <kratikagupta920@gmail.com>
2025-08-10 16:57:23 +0530

    create routerse

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,27 @@
-import { useState } from 'react'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import SingUp from "./pages/SingUp";
+import Projects from "./pages/Projects";
+import SingIn from "./pages/SingIn";
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1 className='text-3xl bg-amber-400'>app</h1>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+       
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path="/sing-up" element={<SingUp/>} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path="/sign-in" element={<SingIn/>} />
+        </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
